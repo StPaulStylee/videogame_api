b497615fd82fc4f67a6ac1c84f9d51cad714397a
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-01-01 09:27:24 -0600

    wishlist and favorites buttons added

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 
 //app.use('/route', routerName);
 
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/views/index.html'));
 });
 
