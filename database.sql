281baf5ae3d4641acde39495d1ce745d1584de33
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-01-01 10:34:28 -0600

    created favorites table

---
+++ database.sql
@@ -0,0 +1,10 @@
+/*1. Create Favorites Table */
+CREATE TABLE favorites (
+id SERIAL PRIMARY KEY,
+  title varchar(120) NOT NULL,
+  description varchar(250),
+  release_date timestamp,
+  platforms varchar ARRAY,
+  game_rating varchar ARRAY,
+  game_image varchar(120)
+);
--- public/scripts/services/favorites.service.js
+++ public/scripts/services/favorites.service.js
@@ -6,9 +6,9 @@ function FavoriteService($http) {
 
   service.addFavorite = function(data) {
     service.newFavorite = {
-      name: data.name,
+      title: data.name,
       description: data.deck,
-      release: data.original_release_date,
+      release_date: data.original_release_date,
       platforms: data.platforms,
       game_rating: data.original_game_rating,
       game_image: data.image.thumb_url
