6d28b29501ef40b04eb07f1619e08bba963c960d
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2016-12-31 19:55:06 -0600

    config updated for new partials to be created

--- public/scripts/app.config.js
+++ public/scripts/app.config.js
@@ -1,4 +1,19 @@
-// angular.module('videoGameApp')
-//        .config(function($routeProvider, $locationProvider){
-//
-//        });
+angular.module('videoGameApp')
+       .config(function($routeProvider, $locationProvider){
+         $routeProvider.when('/home', {
+           templateUrl: 'views/home.html',
+           controller: 'videoGameAPIController as api'
+         }).when('/favorites', {
+           templateUrl: 'views/favorites.html',
+           controller: 'favoritesController as favorite'
+         }).when('/wishlist', {
+           templateUrl: 'views/wishlist.html',
+           controller: 'wishlistController as wish'
+         }).when('/your_rankings', {
+           templateUrl: 'views/your_rankings.html',
+           controller: 'rankingController as rank'
+         }).otherwise({
+           redirectTo: '/home'
+         });
+         $locationProvider.html5Mode(true);
+       });
